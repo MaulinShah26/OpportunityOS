bc4fdedd132b155f26ec73805dcec6b2d33f2b5a
Maulin S <22667469+MaulinShah26@users.noreply.github.com>
2026-07-15 12:22:09 +0530

    Expose evaluation contracts

---
+++ src/opportunityos/evaluation/__init__.py
@@ -0,0 +1,25 @@
+from opportunityos.evaluation.models import (
+    CreateEvaluationDatasetRequest,
+    EvaluationCase,
+    EvaluationCaseResult,
+    EvaluationDataset,
+    EvaluationDatasetCollection,
+    EvaluationDatasetSummary,
+    EvaluationMetrics,
+    EvaluationReport,
+    EvaluationRunCollection,
+    EvaluationRunSummary,
+)
+
+__all__ = [
+    "CreateEvaluationDatasetRequest",
+    "EvaluationCase",
+    "EvaluationCaseResult",
+    "EvaluationDataset",
+    "EvaluationDatasetCollection",
+    "EvaluationDatasetSummary",
+    "EvaluationMetrics",
+    "EvaluationReport",
+    "EvaluationRunCollection",
+    "EvaluationRunSummary",
+]
